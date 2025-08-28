3bcffa8f31ad570eebdbde788ed5242109683dd3
sauragonaa <28881856+sauragonaa@users.noreply.github.com>
2025-08-28 09:11:25 -0600

    delete dead code

--- src/caption_flow/utils/__init__.py
+++ src/caption_flow/utils/__init__.py
@@ -1,6 +1,5 @@
 """Utility modules for CaptionFlow."""
 
 from .dataset_loader import DatasetLoader
-from .shard_tracker import ShardTracker
 from .chunk_tracker import ChunkTracker
 from .caption_utils import CaptionUtils
