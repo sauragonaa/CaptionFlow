6ec4d21ddfdb540e6a4ba8ad5d727d067e887c6f
sauragonaa <28881856+sauragonaa@users.noreply.github.com>
2025-08-29 15:04:35 -0600

    Revert "load with puhu"
    This reverts commit 4bdc70356e1dfaff56fc4076615e4f8d535eda4a.

--- src/caption_flow/processors/webdataset.py
+++ src/caption_flow/processors/webdataset.py
@@ -938,8 +938,6 @@ class WebDatasetWorkerProcessor(WorkerProcessor):
                             "_key": key,
                         },
                     )
-                else:
-                    image = Image.open(image_data)
 
                 job_id = f"{shard_name}:chunk:{chunk_index}:idx:{idx}"
 
@@ -962,7 +960,7 @@ class WebDatasetWorkerProcessor(WorkerProcessor):
 
                 # Prepare item for captioning
                 yield {
-                    "image": image,
+                    "image": image_data,
                     "item_key": str(key),  # Ensure string copy
                     "item_index": idx,
                     "metadata": clean_metadata,
